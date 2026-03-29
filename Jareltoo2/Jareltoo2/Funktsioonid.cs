a68d218f8aa90495e46e6b1e9f4be93bc7dccca3
Andrii611 <ffgggfegujd@gmail.com>
2026-03-29 18:17:11 +0300

    Jareltoo2

---
+++ Jareltoo2/Jareltoo2/Funktsioonid.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jareltoo2
+{
+    internal class Funktsioonid
+    {
+        public static void TaksoKalkulaator()
+        {
+            Console.Write("Sisesta sisseistumistasu (€): ");
+            double sisseistumistasu = double.Parse(Console.ReadLine());
+
+            Console.Write("Sisesta kilomeetri hind (€/km): ");
+            double kmHind = double.Parse(Console.ReadLine());
+
+            Console.Write("Sisesta läbitud kilomeetrid (km): ");
+            double kilomeetrid = double.Parse(Console.ReadLine());
+
+            double kogumaksumus = sisseistumistasu + (kmHind * kilomeetrid);
+
+            Console.WriteLine($"Taksosõidu kogumaksumus: {kogumaksumus:F2} €");
+        }
+
+        public static void HindaKellaaega(string kellaaeg)
+        {
+            if (kellaaeg.Length != 8)
+            {
+                Console.WriteLine("Vigane kellaaja formaat!");
+                return;
+            }
+
+            string tunnid = kellaaeg.Substring(0, 2);
+            string minutid = kellaaeg.Substring(3, 2);
+            string sekundid = kellaaeg.Substring(6, 2);
+
+            Console.WriteLine($"Kell on täpselt {tunnid} tundi, {minutid} minutit ja {sekundid} sekundit.");
+        }
+
+        public static void TaringuStatistika()
+        {
+            Random juhuslik = new Random();
+            List<int> tulemused = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tulemused.Add(juhuslik.Next(1, 7));
+            }
+
+            Console.WriteLine("Viske tulemused: " + string.Join(", ", tulemused));
+
+            int kuuedArv = 0;
+            foreach (int tulemus in tulemused)
+            {
+                if (tulemus == 6)
+                {
+                    kuuedArv++;
+                }
+            }
+
+            Console.WriteLine($"Numbrit 6 visati {kuuedArv} korda.");
+        }
+
+        public static Tuple<double, double> ArvutaRing(double raadius)
+        {
+            double pindala = Math.PI * raadius * raadius;
+            double umbermoot = 2 * Math.PI * raadius;
+
+            return new Tuple<double, double>(pindala, umbermoot);
+        }
+    }
+}
---
+++ Jareltoo2/Jareltoo2/StartPage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Jareltoo2
+{
+    public class StartPage
+    {
+        static void Main(string[] args)
+        {
+            bool tootan = true;
+
+            while (tootan)
+            {
+                Console.WriteLine("\nVali tegevus:");
+                Console.WriteLine("1 - Taksosõidu kalkulaator");
+                Console.WriteLine("2 - Kellaaja eraldaja");
+                Console.WriteLine("3 - Täringu statistika");
+                Console.WriteLine("4 - Ringi arvutused");
+                Console.WriteLine("0 - Välju");
+                Console.Write("Sinu valik: ");
+
+                string valik = Console.ReadLine();
+
+                switch (valik)
+                {
+                    case "1":
+                        Funktsioonid.TaksoKalkulaator();
+                        break;
+
+                    case "2":
+                        Console.Write("Sisesta kellaaeg (HH:MM:SS): ");
+                        string kellaaeg = Console.ReadLine();
+                        Funktsioonid.HindaKellaaega(kellaaeg);
+                        break;
+
+                    case "3":
+                        Funktsioonid.TaringuStatistika();
+                        break;
+
+                    case "4":
+                        Console.Write("Sisesta ringi raadius: ");
+                        double raadius = double.Parse(Console.ReadLine());
+
+                        Tuple<double, double> tulemus = Funktsioonid.ArvutaRing(raadius);
+
+                        Console.WriteLine($"Pindala: {Math.Round(tulemus.Item1, 2)} ruutuhikut");
+                        Console.WriteLine($"Umbermoot: {Math.Round(tulemus.Item2, 2)} uhikut");
+                        break;
+
+                    case "0":
+                        Console.WriteLine("Head aega!");
+                        tootan = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Vigane valik, proovi uuesti.");
+                        break;
+                }
+            }
+        }
+    }
+}
